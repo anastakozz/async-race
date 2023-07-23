2ea5f2102c8452a947a46ae83f97edf14f86202f
anastakozz <117598324+anastakozz@users.noreply.github.com>
2023-07-23 14:48:51 +0300

    refactor: delete comments

--- async-race/src/api/updateGarageCar.ts
+++ async-race/src/api/updateGarageCar.ts
@@ -38,24 +38,3 @@ export default async function activateUpdateCar(event: Event) {
   const updateCarButton = document.querySelector(".update-btn");
   updateCarButton?.addEventListener("click", updateGarageCar);
 }
-
-// import { createCar } from "./setApi";
-// import isInputElement from "../utils/isInputElement";
-// import RaceView from "../view/garage_components/raceView";
-
-// export default async function createGarageCar(
-//   raceView: RaceView
-// ): Promise<void> {
-//   const createCarBlock = document.querySelector(".createCar-div");
-//   const newName = createCarBlock?.children[0];
-//   const newColor = createCarBlock?.children[1];
-//   if (isInputElement(newName) && isInputElement(newColor)) {
-//     if (newName.value === "") {
-//       alert("Please, enter a car name");
-//     } else {
-//       const data = { name: newName.value, color: newColor.value };
-//       await createCar(data);
-//       await raceView.generateCarTrack(data);
-//     }
-//   }
-// }
